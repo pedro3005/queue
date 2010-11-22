913797578c5e04220ccece94f0f54867b654f10d
pedro3005 <pedrorm2000@gmail.com>
2010-11-22 16:43:57 -0300

    Removed fluff

--- queue.py
+++ queue.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import sys
 import cPickle
@@ -6,7 +6,7 @@ import cPickle
 class QueueError(Exception):
     pass
 
-class Queue:
+class Queue(object):
     def __init__(self, *args):
         self.queue = []
         for arg in args:
@@ -23,17 +23,12 @@ class Queue:
             return self.queue.pop()
         except IndexError:
             raise QueueError("Empty queue!")
+    def to_list(self):
+        return [self.next() for i in range(len(self))]
     def __str__(self):
         return repr(self.queue)
     def __len__(self):
         return len(self.queue)
-
-def queue_to_list(queue):
-    return [queue.next() for i in range(len(queue))]
-
-def queue(qlist):
-    '''Converts a list object to a queue.'''
-    return Queue(*qlist)
 
 if __name__ == "__main__":
     
@@ -41,30 +36,18 @@ if __name__ == "__main__":
         main, pending = cPickle.load(open(".queue_data"))
     except: # failed somehow to open and de-serialize the data.
             # could be practically any exception, so it is a bare 'except: '
-        print '''Failed to load the queue data, creating a new one...
-Everything's fine, though you did lose anything you might have stored.'''
         main = Queue()
         pending = Queue()
-    finally:
-        outfd = open(".queue_data", "w")
     
     sys.argv.pop(0) # remove the useless './queue.py' part
     argc = len(sys.argv)
     
     if argc == 0:
-        print '''Welcome to queue.py! Commands:
-    add item1 [item2, item3, ...] -- adds the items to the queue
-    next -- grabs an item from the queue and puts it as pending
-    reset -- moves all pending items back to the queue
-    done [item1, item2, ...] -- without arguments, removes the first pending item.
-    print -- prints your queues (main and pending)
-    clear -- clears the main queue'''
+       sys.exit(1)
         
     elif sys.argv[0] == "add":
         if argc == 1:
-            print '''You're doing it wrong!
-The right way is:
-    ./queue.py add item1 [item2, item3, ...]'''
+            sys.exit(1)
         else:
             for item in sys.argv[1:]:
                 main.put(item)
@@ -75,13 +58,13 @@ The right way is:
             pending.put(next_key)
             print "{0}".format(next_key)
         except QueueError:
-            print "Queue is empty! Try adding something"
+            sys.exit(1)
     
     elif sys.argv[0] == "reset":
         if len(pending) < 1:
-            print "There is nothing on the pending queue."
+            sys.exit(1)
         else:
-            main = Queue(*(queue_to_list(pending) + queue_to_list(main)))
+            main = Queue(*(pending.to_list() + main.to_list()))
     
     elif sys.argv[0] == "done":
         if argc == 1:
@@ -91,16 +74,15 @@ The right way is:
                 try:
                     pending.remove(item)
                 except QueueError:
-                    print "Could not find {0}".format(item)
+                    pass
     
     elif sys.argv[0] == "print":
-        print '''Here is your main queue:
-{0}
-And here are your pending items:
-{1}'''.format(main, pending)
+        print ", ".join(main.queue)
+        print ", ".join(pending.queue)
 
     elif sys.argv[0] == "clear":
         main = Queue()
     
+    outfd = open(".queue_data", "w")
     cPickle.dump([main, pending], outfd)
     outfd.close()
